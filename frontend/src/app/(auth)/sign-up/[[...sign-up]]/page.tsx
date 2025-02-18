04ee31632cbe6c525e4c5f6a4c9153ad37202618
preet patel <preetpatelofficial004@gmail.com>
2025-02-17 20:35:22 -0500

    Setting Up Frontend

---
+++ frontend/src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,20 @@
+import { SignUp } from '@clerk/nextjs';
+
+export default function Page() {
+  return (
+    <div className='flex items-center justify-center w-full h-screen'>
+      <SignUp 
+      appearance={{
+        elements: {
+        //   card: "bg-white shadow-md rounded-lg",
+        //   headerSubtitle: "hidden", // Hide the default subtitle if you don't want it
+        //   logoBox: "mx-auto mb-4", // Center the logo and add margin
+          formButtonPrimary: "bg-slate-700 hover:bg-slate-800", // Customize button color
+        },
+        
+      }}
+      />
+      
+    </div>
+  );
+}
